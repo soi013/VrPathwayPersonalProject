63d807dab6f58ae1beb8fd2e36a92adf509e0b55
soi013 <soi2flatline@gmail.com>
2023-04-16 21:17:18 +0900

    FixFire

--- Assets/FireLantern.cs
+++ Assets/FireLantern.cs
@@ -13,6 +13,11 @@ public class FireLantern : MonoBehaviour
         if (!currentTrail)
         {
             currentTrail = Instantiate(trailPrefab, transform.position, transform.rotation, transform);
+
+            TrailRenderer trailRenderer = currentTrail.GetComponent<TrailRenderer>();
+            //trailRenderer.startColor = Color.red;
+            //trailRenderer.endColor = Color.red;
+            //trailRenderer.time = 5000;
         }
     }
 
